ba9db0bc9f4439573d2538ae757269b6b8d63d6c
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2017-10-04 19:05:41 -0500

    Update github-update.sh

--- bin/github-update.sh
+++ bin/github-update.sh
@@ -5,6 +5,15 @@ GITHUBARCHIVE="/tmp/master.zip"
 #fetch the latest files from gethub
 curl -L https://github.com/shepner/UNIX/archive/master.zip > $GITHUBARCHIVE
 
+
+#extract stuff
+
+unzip -jo $GITHUBARCHIVE Docker-master/UNIX/.* -d ~/
+chmod -R 750 .vim
+chmod 700 .vimrc
+chmod 640 .profile
+chmod 600 .inputrc
+
 unzip -jo $GITHUBARCHIVE Docker-master/UNIX/bin/* -d ~/bin
 chmod -R 750 ~/bin
 
@@ -12,3 +21,17 @@ unzip -jo $GITHUBARCHIVE Docker-master/UNIX/etc/* -d ~/etc
 chmod 750 ~/etc
 chmod 640 ~/etc/*
 
+case `uname` in
+  Darwin)
+    echo OSX
+    ;;
+  FreeBSD)
+    echo FreeBSD
+    ;;
+  Linux)
+    echo Linux
+    ;;
+  *)
+    echo unknown OS
+    ;;
+esac
